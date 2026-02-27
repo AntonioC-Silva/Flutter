4b419439b3fa905c583ea5b46a2f48a141e3e797
Silva Antonio (CaP/ETS3) <inl4ca@bosch.com>
2026-02-27 11:06:25 -0300

    'quase'

--- tree_fluter/lib/screens/Boleto.dart
+++ tree_fluter/lib/screens/Boleto.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'dart:io';
 
+import 'package:tree_fluter/main.dart';
+
 
 class Boleto extends StatefulWidget {
   const Boleto({super.key});
@@ -13,9 +15,41 @@ class Boleto extends StatefulWidget {
 class _BoletoState extends State<Boleto> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Boleto()
+    return Scaffold(
+        appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.arrow_back, color: Colors.white,), onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => TelaInicial()),);
+        },),
+        centerTitle: true,
+        title: Text("BOLETO", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.orange,),
+
+        body: Center( child: Column(
+          children: [
+
+              SizedBox(height: 60),
+              TextButton.icon(
+                style: TextButton.styleFrom( 
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
+                elevation: 10,
+                shadowColor: Colors.black.withOpacity(1),
+                backgroundColor: Colors.orange,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(10)),
+                ),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Boleto()));
+                  },icon: Icon(Icons.cameraswitch_outlined, color: Colors.white, size: 20,), 
+                  label: Text("Scanear Boleto", style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold,),),),
+
+
+
+          ],
+        ),),
+
+
+
+
+
 
     );
   }
--- tree_fluter/lib/screens/Cartao.dart
+++ tree_fluter/lib/screens/Cartao.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'dart:io';
 
+import 'package:tree_fluter/main.dart';
+
 
 class Cartao extends StatefulWidget {
   const Cartao({super.key});
@@ -13,10 +15,50 @@ class Cartao extends StatefulWidget {
 class _CartaoState extends State<Cartao> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Cartao()
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.arrow_back, color: Colors.white,), onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => TelaInicial()),);
+        },),
+        centerTitle: true,
+        title: Text("CARTÃO", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.orange,),
+
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 60),
+              Text("Peça já o seu!",style: TextStyle(color: Colors.black, fontWeight: FontWeight.normal, fontSize: 20),),
+
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 300,
+                height: 500,
+                decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/page_c.png"), fit: BoxFit.cover), borderRadius: BorderRadius.circular(15)),
+                
+              ),
+
+              SizedBox(height: 60),
+              TextButton(
+                style: TextButton.styleFrom( 
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
+                elevation: 10,
+                shadowColor: Colors.black.withOpacity(1),
+                backgroundColor: Colors.orange,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(10)),
+                ),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Cartao()));
+                  }, child: Text("Solicitar", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),), ),
+ 
+
+            ],
+          ),
+        ),
+      
+     
 
     );
   }
 }
+
